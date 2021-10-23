8adb33e24ab628d3578d243455aab31e9e1d9182
Brandon Simon <brandon.simon2@gmail.com>
2021-10-23 15:11:37 -0400

    updated search history code

--- searchhistory.rb
+++ searchhistory.rb
@@ -1,3 +1,16 @@
+# the code below fetches the HTML code from the url in the 'url' variable and places it in the "TempWebsiteURLs" folder
+# system("wget #{url} -P TempWebsiteURLs ")
+puts "=> Welcome to the internet search history widget!"
+puts "=> Which keyword or phrase would you like to search for?"
+
+keywords = gets.chomp.downcase
 url = "https://stackoverflow.com/questions/3742983/how-to-get-the-contents-of-a-webpage-in-a-shell-variable"
 
-system("wget #{url} -P TempWebsiteURLs ")
+def keyword_finder(keywords, url)
+  system('clear')
+  counter = `curl -s #{url}`.downcase.scan(keywords).length
+  puts "your keyword '#{keywords}' appears #{counter} times in the following url:"
+  puts "#{url}"
+end
+
+keyword_finder(keywords,url)

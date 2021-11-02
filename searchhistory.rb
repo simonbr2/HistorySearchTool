3b5d4625c0a6cda9abb40f7d11da9acd5ca83994
Brandon Simon <brandon.simon2@gmail.com>
2021-11-02 12:51:58 -0400

    updated search program to store results in a hash instead of array. hash is now sorted by number of hits on final output

--- searchhistory.rb
+++ searchhistory.rb
@@ -6,24 +6,25 @@ puts "=> Which keyword or phrase would you like to search for?"
 
 keywords = gets.chomp.downcase
 
-def keyword_finder(keywords, url, array)
-  system('clear')
+def keyword_finder(keywords, url, hash)
+  # system('clear')
   counter = `curl -s #{url}`.downcase.scan(keywords).length
-  puts "your keyword '#{keywords}' appears #{counter} times in the following url:"
-  puts "#{url}"
+  # puts "your keyword '#{keywords}' appears #{counter} times in the following url:"
+  # puts "#{url}"
   if counter > 0
-    array << "#{url}"
+    hash["#{url}"] = counter
   end
-  p array
 end
 
 def history_iterator(keywords)
-  positive_hits = []
+  positive_hits = Hash.new
   history = `(cd ~/.mozilla/firefox; for a in */places.sqlite; do sqlite3 $a "select h.visit_date,p.url from moz_historyvisits as h, moz_places as p where p.id == h.place_id order by h.visit_date"; done)`.split('|')
   history.each do |link|
+    # p positive_hits.sort { |a1,a2| a2[1] <=> a1[1] }
     keyword_finder(keywords, link, positive_hits)
-    enter_to_continue
+    # enter_to_continue
   end
+  p positive_hits.sort { |a1,a2| a2[1] <=> a1[1]  }
 end
 
 def enter_to_continue
